b78f650aa9df4b3abf19b79e0a7cad3abd15d218
Artur Linux Acer <nevesdev.ti@gmail.com>
2024-01-08 22:43:17 -0300

    first commit

---
+++ src/test/java/com/lambdaAndSpring/screenmatch/CourseLambdaAndSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lambdaAndSpring.screenmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseLambdaAndSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
